cc4b738d006d05c86c93a0704bc7733fdf0aab3a
Roberto Díaz <robertodimen@gmail.com>
2026-06-19 11:21:09 +0200

    Consistency in naming and use

--- src/main/java/cart/ai/shopping/domain/ports/security/PasswordEncoderPort.java
+++ src/main/java/cart/ai/shopping/domain/ports/security/PasswordEncoderPort.java
@@ -3,7 +3,7 @@
  * Licensed under the GNU General Public License v3.0. See LICENSE for details.
  */
 
-package cart.ai.shopping.domain.ports.identity.services;
+package cart.ai.shopping.domain.ports.security;
 
 /**
  * @author Roberto Díaz
